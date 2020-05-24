84eef0bc1a9e091bc15886812adcbce1470a58a6
Michael Kuc <michaelkuc6@gmail.com>
2020-05-24 20:09:51 +0100

    build(PyPI): update package info
    Bumped package info in order to update the package on PyPI.

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup
 
 setup(
     name="conventional_JIRA",
-    version="0.1.0",
+    version="0.2.1",
     py_modules=["cz_conventional_JIRA"],
+    author = 'Michael Kuc',
+    author_email = 'michaelkuc6@gmail.com',
+    url = 'https://github.com/Crystalix007/conventional_jira',
     license="MIT",
     long_description="A variation of conventional commits enforcing a JIRA issue as the scope.",
     install_requires=["commitizen"],
